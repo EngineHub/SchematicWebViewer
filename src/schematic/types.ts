536abe95cebfc5b13595c95d4de93b8a591d5515
Matthew Miller <mnmiller1@me.com>
2019-09-19 22:49:59 +1000

    chore: refactor schematic loading

---
+++ src/schematic/types.ts
@@ -0,0 +1,64 @@
+import { Block, BlockVector3 } from '../types';
+import { make3DArray } from '../util/array';
+
+export class Schematic implements Iterable<BlockVector3> {
+    width: number;
+    height: number;
+    length: number;
+    blocks: Block[][][];
+
+    constructor(width: number, height: number, length: number) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+
+        this.blocks = make3DArray(width, height, length);
+    }
+
+    contains(pos: BlockVector3): boolean {
+        return (
+            pos.x < this.width &&
+            pos.y < this.height &&
+            pos.z < this.length &&
+            pos.x >= 0 &&
+            pos.y >= 0 &&
+            pos.z >= 0
+        );
+    }
+
+    getBlock(pos: BlockVector3): Block | undefined {
+        if (!this.contains(pos)) {
+            return undefined;
+        }
+        return this.blocks[pos.x][pos.y][pos.z];
+    }
+
+    setBlock(pos: BlockVector3, block: Block): void {
+        if (!this.contains(pos)) {
+            return;
+        }
+        this.blocks[pos.x][pos.y][pos.z] = block;
+    }
+
+    [Symbol.iterator](): Iterator<BlockVector3> {
+        let nextX = 0;
+        let nextY = 0;
+        let nextZ = 0;
+
+        return {
+            next: function() {
+                const answer = { nextX, nextY, nextZ };
+                if (++nextX > this.width) {
+                    nextX = 0;
+                    if (++nextZ > this.length) {
+                        nextZ = 0;
+                        if (++nextY > this.height) {
+                            nextX = -1;
+                        }
+                    }
+                }
+                return { value: answer, done: nextX === -1 };
+            }.bind(this)
+        };
+    }
+}
